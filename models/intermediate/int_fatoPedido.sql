9cfe68207c015c9376598c2fb195289ba3d72637
Gabriela Costa <gabrielasmorais01@gmail.com>
2024-10-22 19:44:51 +0000

    Finalizado todos os testes e documentação.

--- models/intermediate/int_fatoPedido.sql
+++ models/intermediate/int_fatoPedido.sql
@@ -1,7 +1,8 @@
 WITH 
     fontePedidos AS (
         SELECT 
-              vendaHeader.pk_idPedido
+              UUID_STRING() AS unique_id
+            , vendaHeader.pk_idPedido
             , vendaDetalhes.pk_idPedidoDetalhe
             , vendaHeader.dataCompra
             , produto.pk_idProduto
@@ -9,23 +10,18 @@ WITH
             , tipoCartao.pk_idTipoCartao
             , vendaHeader.statusPedido
             , endereco.pk_idEndereco
-            , CAST(vendaDetalhes.quantidadePedido AS DECIMAL (10,4)) AS QUANTIDADEPEDIDO
-            , CAST(vendaDetalhes.precoUnitario AS DECIMAL (10,4)) AS PRECOUNITARIO
+            , CAST(vendaDetalhes.quantidadePedido AS DECIMAL(10,4)) AS quantidadePedido
+            , CAST(vendaDetalhes.precoUnitario AS DECIMAL(10,4)) AS precoUnitario
             , vendaDetalhes.descontoUnitario
-
         FROM {{ ref('stg_salesorderdetail') }} AS vendaDetalhes
-        LEFT JOIN {{ ref('stg_salesorderheader') }} AS vendaHeader
+        JOIN {{ ref('stg_salesorderheader') }} AS vendaHeader
             ON vendaDetalhes.pk_idPedido = vendaHeader.pk_idPedido
-
         LEFT JOIN {{ ref('stg_produto') }} AS produto
             ON vendaDetalhes.fk_idProduto = produto.pk_idProduto
-
         LEFT JOIN {{ ref('stg_cliente') }} AS cliente
             ON vendaHeader.fk_idCliente = cliente.pk_idCliente
-
         LEFT JOIN {{ ref('stg_tipocartao') }} AS tipoCartao
             ON vendaHeader.fk_idTipoCartao = tipoCartao.pk_idTipoCartao
-
         LEFT JOIN {{ ref('stg_cidade') }} AS endereco
             ON vendaHeader.BILLTOADDRESSID = endereco.pk_idEndereco
     ) 
